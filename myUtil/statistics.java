5167c12f1dfd8b39a605d9856bfbde0d12d480d6
jdr357 <jdr357@txstate.edu>
2025-01-22 13:31:03 -0600

    Create statistics.java

---
+++ myUtil/statistics.java
@@ -0,0 +1,34 @@
+package myUtil;
+
+public class statistics{
+
+    //Average function
+    public static double getAverage(int[] _arr){
+
+        return 1.00; //Place Holders just to get rid of errors. Must change!!!
+    }
+
+    //Sort function
+    static void sort(int[] _arr){
+
+    }
+
+    //Swap function
+    static void swap(int[] _arr, int _pos0, int _pos1){
+
+    }
+
+    //Median function
+    public static int getMedian(int[] _arr){
+
+        return 1; //Place Holders just to get rid of errors. Must change!!!
+    }
+
+    //Main function
+    public static void main(String[] args) {
+        
+        a
+
+    }
+
+}
